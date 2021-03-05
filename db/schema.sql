1d8f28c6912a03341b1a9220238d7c0d7283a453
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-03-04 20:08:33 -0800

    added schema for employees db, role table, department table, employee table

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+-- DROP DATABASE IF EXISTS employees_db;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+
+CREATE DATABASE employees_db;
+USE employees_db;
+
+CREATE TABLE department (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL
+);
+
+CREATE TABLE employee(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
+);
